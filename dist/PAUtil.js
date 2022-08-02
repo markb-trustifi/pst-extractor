0e5253671c02ea3858b326858262a136cf199c77
kenjiuno <ku@digitaldolphins.jp>
2022-08-02 19:00:01 +0900

    npm packaged
    Publishing as
    https://github.com/HiraokaHyperTools/pst-extractor.git#dist/v0.1.0-SNAPSHOT

--- dist/PAUtil.js
+++ dist/PAUtil.js
@@ -2,29 +2,6 @@
 /**
  * PST adapter utilities
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -118,9 +95,6 @@ function processNameToIDMap(nameToIdMapDescriptorNode, resolver) {
         const stringNameToIdByteView = new DataView(stringMapEntries);
         const stringNameToIdByteBuffer = Buffer.from(stringMapEntries);
         const nodeMap = new NodeMap_class_1.NodeMap();
-        (yield Promise.resolve().then(() => __importStar(require("fs")))).writeFileSync("C:/A/A1", nameToIdByte);
-        (yield Promise.resolve().then(() => __importStar(require("fs")))).writeFileSync("C:/A/A2", stringNameToIdByte);
-        (yield Promise.resolve().then(() => __importStar(require("fs")))).writeFileSync("C:/A/A3", guids);
         // process the entries
         for (let x = 0; x + 8 < nameToIdByte.length; x += 8) {
             const key = nameToIdByteView.getUint32(x, true);
