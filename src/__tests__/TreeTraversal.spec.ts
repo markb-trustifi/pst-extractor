9e0674c09a2a36c504c136e2f8f47c0ee02a11d6
kenjiuno <ku@digitaldolphins.jp>
2022-08-04 15:33:36 +0900

    Adding TreeTraversal.spec.ts

---
+++ src/__tests__/TreeTraversal.spec.ts
@@ -0,0 +1,59 @@
+import { PSTFile } from '../PSTFile.class'
+import { PSTFolder } from '../PSTFolder.class'
+import { openPstFile } from '../openPstFile'
+import { resolve, extname } from 'path';
+import fs from 'fs';
+
+// yarn test:unit --coverage false -t "tree traversal tests"
+
+// add dir list having pst/ost files
+const sourceDirs: string[] = [
+  // './src/__tests__/testdata/',
+];
+
+// add file list of pst/ost files
+const sourceFiles: string[] = [
+  // './src/__tests__/testdata/mtnman1965@outlook.com.ost',
+];
+
+describe('tree traversal tests', () => {
+  sourceDirs.forEach(
+    dir => {
+      for (let name of fs.readdirSync(resolve(dir))) {
+        const fileExt = extname(name).toLowerCase();
+        if (fileExt === ".pst" || fileExt === ".ost") {
+          sourceFiles.push(resolve(dir, name));
+        }
+      }
+    }
+  );
+
+  it.each(sourceFiles)("scan", async (sourceFile: string) => {
+    await scan(sourceFile);
+  });
+});
+
+async function scan(sourceFile: string): Promise<void> {
+  const pstFile = await openPstFile(sourceFile);
+  try {
+    await scanTree((await pstFile.getRootFolder()), 0);
+  }
+  finally {
+    pstFile.close();
+  }
+}
+
+async function scanTree(folder: PSTFolder, depth: number): Promise<void> {
+  function prefix(text: string): string {
+    return "  ".repeat(depth) + text;
+  }
+  for (let subFolder of (await folder.getSubFolders())) {
+    //console.log(prefix(`@ ${subFolder.displayName}`));
+    await scanTree(subFolder, depth + 1);
+  }
+  for (let item of (await (await folder.getEmails()))) {
+    //console.log(prefix(`- ${item.displayName}`));
+    (await item.getAttachments());
+    (await item.getRecipients());
+  }
+}
