f78538fd0595031b5214abbca9270f424ca86674
MarkBabayev <mbabayev@trustificorp.com>
2025-01-02 15:49:08 +0200

    fixes

--- src/FasterEmail.ts
+++ src/FasterEmail.ts
@@ -3,6 +3,5 @@ import { PSTMessage } from "./PSTMessage.class";
 export interface FasterEmail {
   displayName: string;
   messageClass: string;
-  messageId: string;
   getMessage: () => Promise<PSTMessage>;
 }
